5074dfd1f3beedf179b9854e0b8099e7561f9642
amazon <amazon@amazons-MacBook-Pro.local>
2019-05-28 10:42:38 -0400

    asd

--- firstproject/test2.py
+++ firstproject/test2.py
@@ -10,7 +10,7 @@ def solution(A):
     max=-100 #init
     sizeA = len(A)
     for i in range(0,sizeA):
-        for j in range(i,sizeA):
+        for j in range(i+1,sizeA):#I made a mistake -- I wrote "range(i,sizeA)". Correct is "range(i+1,sizeA)"
             sum = A[i]+A[j]+distance(i,j)
             if(max<sum):
                 max=sum
